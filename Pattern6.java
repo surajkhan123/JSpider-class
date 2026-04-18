1bd04cc32e1fa33450833edd23c2aa7f8d48de3f
Your Name <you@example.com>
2026-04-19 01:20:09 +0530

    i

--- Pattern6.java
+++ Pattern6.java
@@ -4,7 +4,7 @@ public class Pattern6 {
     int sp=n-1;
     int st=1;
     for(int i=1;i<=n;i++){
-      for(int j=1;j<=sp;j++) System.out.print(" ");
+      for(int j=1;j<=sp;j++) System.out.print("* ");
       for(int k=1;k<=st;k++){
         System.out.print(" ");
       }
--- Pattern9.java
+++ Pattern9.java
@@ -5,8 +5,7 @@ class Pattern9 {
     for(int i=1;i<=rows;i++)
       {
       for(int j=1;j<=cols;j++){
-        if(i==1|| i==3|| i==5|| (i==2 && j==1)||(i==4 &
-          & j==cols))
+        if(i==1|| i==3|| i==5|| (i==2 && j==1)||(i==4 && j==cols))
         // if(i==1|| i==cols|| j==1|| i==rows/2+1 && j>rows/2 || j==cols && i>rows/2 )
         {
           System.out.print("* ");
